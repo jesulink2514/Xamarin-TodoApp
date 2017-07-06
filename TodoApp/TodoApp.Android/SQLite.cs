3f664140780f45c436ed32ed629ccab79eb4ddd4
Jesus Angulo <jesus.angulo@outlook.com>
2017-07-06 18:58:47 -0500

    Intro to SQLite

---
+++ TodoApp/TodoApp.Android/SQLite.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using SQLite;
+using TodoApp.Infrastructure;
+
+[assembly:Xamarin.Forms.Dependency(typeof(TodoApp.Droid.SQLite))]
+namespace TodoApp.Droid
+{
+    public class SQLite : ISQLite
+    {
+        public SQLiteConnection GetConnection()
+        {
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var dbPath = Path.Combine(path, "todo.db3");
+            return new SQLiteConnection(dbPath);
+        }
+    }
+}
---
+++ TodoApp/TodoApp/Infrastructure/TodoDatabase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using Xamarin.Forms;
+
+namespace TodoApp.Infrastructure
+{
+    public class TodoDatabase
+    {
+        private SQLiteConnection database;
+        private static object _lock = new object();
+
+        public TodoDatabase()
+        {
+            database = DependencyService.Get<ISQLite>().GetConnection();
+            database.CreateTable<TodoItem>();
+        }
+
+        public int Save(TodoItem todoItem)
+        {
+            lock (_lock)
+            {
+                return 0;
+            }
+        }
+    }
+}
