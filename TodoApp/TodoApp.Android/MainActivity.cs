564dc498c96eeb2172ef78a9504c93067f7aa574
Jesus Angulo <jesus.angulo@outlook.com>
2017-07-12 22:49:09 -0500

    Finished

--- TodoApp/TodoApp.Android/MainActivity.cs
+++ TodoApp/TodoApp.Android/MainActivity.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-
+using System.IO;
 using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Environment = System.Environment;
 
 namespace TodoApp.Droid
 {
@@ -19,9 +20,36 @@ namespace TodoApp.Droid
 
             base.OnCreate(bundle);
 
+#if DEBUG
+            CopyDatabase();
+#endif
+
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
         }
+#if DEBUG
+        private async void CopyDatabase()
+        {
+            var path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            var dbPath = Path.Combine(path, "todo.db3");
+
+            var external = Android.OS.Environment
+                .GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryDownloads).AbsolutePath;
+
+            var externalPath = Path.Combine(external, "todo-backup.db3");
+
+            if(!File.Exists(dbPath))return;
+
+            var origin = File.OpenRead(dbPath);
+            var destino = File.Exists(externalPath) ? File.OpenWrite(externalPath) : File.Create(externalPath);
+
+            using (origin)
+            using (destino)
+            {
+                await origin.CopyToAsync(destino);
+            }
+        }
+#endif
     }
 }
 
