b3467bee7dfd01ecffd2422537ab93d0ff3581cf
Jesus Angulo <jesus.angulo@outlook.com>
2017-07-12 20:28:03 -0500

    3.10 Recursos  y conversiones

---
+++ TodoApp/TodoApp/Helpers/DateTimeConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace TodoApp.Helpers
+{
+    public class DateTimeConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var date = (DateTime)value;
+            return date.ToString("dd/MM/yyyy hh:mm:ss");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
